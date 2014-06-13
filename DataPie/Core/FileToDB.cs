109a9f945de443fe9eadab981b9024f7d2391ed3
yfl8910 <yfl8910@163.com>
2014-06-13 22:41:53 +0800

    DataPieV3.8

---
+++ DataPie/Core/FileToDB.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Data;
+using System.Data.OleDb;
+using System.IO;
+using System.Collections.Generic;
+
+namespace DataPie.Core
+{
+    public class FileToDB
+    {
+
+       // Excel2003、Excel2007、ACCESS2007、CSV文件导入
+        public static DataTable GetDataTableFromFile(string path, string tname)
+        {
+            string ace = "Microsoft.ACE.OLEDB.12.0";
+            string jet = "Microsoft.Jet.OLEDB.4.0";
+            string xl2007 = "Excel 12.0 Xml";
+            string xl2003 = "Excel 8.0";
+            string imex = "IMEX=1";
+            string hdr = "Yes";
+            string conn = "Provider={0};Data Source={1};Extended Properties=\"{2};HDR={3};{4}\";";
+            string select = "";
+            string ext = Path.GetExtension(path);
+            OleDbDataAdapter oda;
+            DataTable dt = new DataTable(tname);
+            switch (ext.ToLower())
+            {
+                case ".xlsx":
+                    conn = String.Format(conn, ace, Path.GetFullPath(path), xl2007, hdr, imex);
+                    select = string.Format("SELECT * FROM [{0}$]", tname);
+                    break;
+                case ".xls":
+                    conn = String.Format(conn, jet, Path.GetFullPath(path), xl2003, hdr, imex);
+                    select = string.Format("SELECT * FROM [{0}$]", tname);
+                    break;
+                case ".accdb":
+                    conn = String.Format("Provider=Microsoft.ACE.OLEDB.12.0;Data Source= {0};Persist Security Info=False;", path);
+                    select = string.Format("SELECT * FROM [{0}]", tname);
+                    break;
+                case ".csv":
+                    conn = String.Format(conn, ace, path.Substring(0, path.LastIndexOf('\\')), "text;Excel 12.0", hdr, imex);
+                    select = string.Format("SELECT * FROM [{0}]", Path.GetFileName(path));
+                    break;
+                default:
+                    throw new Exception("File Not Supported!");
+            }
+            OleDbConnection con = new OleDbConnection(conn);
+            con.Open();
+            oda = new OleDbDataAdapter(select, con);
+            oda.Fill(dt);
+            con.Close();
+            return dt;
+        }
+
+
+    
+
+       //从文件夹获取csv文件
+        public static List<FileInfo> GetFilelist(string DirectoryPath, bool rec)
+        {
+            List<FileInfo> fileList = FileManager.FileList(DirectoryPath, rec);
+            List<FileInfo> files = new List<FileInfo>();
+            int n = 0;
+            foreach (FileInfo f in fileList)
+            {
+                if (f.Extension == ".csv")
+                {
+                    n++;
+                    files.Add(f);
+                }
+            }
+            if (n > 0)
+            {
+                return files;
+            }
+            else
+            {
+                return null;
+            }
+
+
+        }
+
+ 
+        public static DataTable GetDataTableFromCSV(FileInfo file)
+        {
+            string tablename = file.Name.Substring(0, file.Name.LastIndexOf("."));
+            return  GetDataTableFromFile(file.FullName, tablename);
+        }
+    }
+}
---
+++ DataPieUI/Common.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataPieUI
+{
+    public class Common
+    {
+
+        #region 弹出FileDialog对话框
+
+        public static string ShowFileDialog(string FileName, string ext)
+        {
+            System.Windows.Forms.SaveFileDialog saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+
+            switch (ext)
+            {
+                case ".xlsx":
+                    saveFileDialog1.Filter = "EXCEL 2007|*.xlsx";
+                    saveFileDialog1.FileName = FileName;
+                    saveFileDialog1.DefaultExt = ".xlsx";
+                    break;
+                case ".accdb":
+                    saveFileDialog1.Filter = "ACCESS数据库|*.accdb";
+                    saveFileDialog1.FileName = FileName;
+                    saveFileDialog1.DefaultExt = ".accdb";
+                    break;
+                case ".csv":
+                    saveFileDialog1.Filter = "CSV文件|*.csv";
+                    saveFileDialog1.FileName = FileName;
+                    saveFileDialog1.DefaultExt = ".csv";
+                    break;
+                default:
+                    break;
+
+            }
+
+            saveFileDialog1.FileName = FileName;
+
+            if (saveFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                return saveFileDialog1.FileName.ToString();
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        #endregion
+
+     
+
+   
+    }
+}
