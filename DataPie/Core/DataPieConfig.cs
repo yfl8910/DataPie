acf5249f51d44bfd2e7755d0c47730d8799c5551
yfl8910 <yfl8910@163.com>
2014-07-26 23:32:51 +0800

    bug修正

---
+++ DataPie/Core/DataPieConfig.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataPie
+{
+   public class DataPieConfig
+    {
+       //默认每个文件存储数65535行
+        public static int RowOutCount = 65535;
+    }
+}
